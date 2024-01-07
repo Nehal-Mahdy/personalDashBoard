47ce5707a615284cd0c1c16dfbf4aeab92b43b26
Nehal-Mahdy <nehalhassan914@yahoo.com>
2024-01-07 21:25:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations:[
+    trigger('routeAnim', [
+
+    ])
+  ]
 })
 export class AppComponent {
   title = 'angular-personal-dashboard';
+
+  prepareRoute(outlet : RouterOutlet){
+    if(outlet.isActivated) {outlet.activatedRoute.snapshot.url}
+
+  }
 }
